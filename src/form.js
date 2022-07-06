4d864cbe5426959febb2e9b1a3b47b0effd56344
Jordan Ulmer <julmer526@gmail.com>
2022-07-06 14:02:14 +0100

    linting added

--- src/form.js
+++ src/form.js
@@ -1,241 +1,240 @@
 // // DOM manipulation for input new todo form and new project form
 
-import { clearProjectList, refreshProjectList } from "."
-import { storeProjects, storeTodos } from "./localStorage"
-import updateMain, { clearTodos } from "./mainContent"
-import Todo, { todoArray } from "./todoObjects"
-import { projectArray } from "./todoObjects"
+import { clearProjectList, refreshProjectList } from '.';
+import { storeProjects, storeTodos } from './localStorage';
+import updateMain, { clearTodos } from './mainContent';
+import Todo, { todoArray, projectArray } from './todoObjects';
 
-//*NEW TODO FORM
-function todoForm (){
-    // container elements for form
-    const form = document.createElement("form")
-    form.classList.toggle("form")
-    const overlayForm = document.createElement("div")
-    overlayForm.classList.toggle("overlayForm")
-    form.appendChild(overlayForm)
-    // close form 'x' button
-    const closeForm = document.createElement("div")
-    closeForm.classList.toggle("closeForm")
-    closeForm.addEventListener("click", () => {
-        toggleForm();
-        removeForm();
-    })
-    closeForm.innerHTML = "&times"
-    overlayForm.appendChild(closeForm)
-    // form header
-    const formHeader = document.createElement("h1")
-    formHeader.textContent = "Add a new ToDo"
-    overlayForm.appendChild(formHeader);
+//* NEW TODO FORM
+function todoForm() {
+  // container elements for form
+  const form = document.createElement('form');
+  form.classList.toggle('form');
+  const overlayForm = document.createElement('div');
+  overlayForm.classList.toggle('overlayForm');
+  form.appendChild(overlayForm);
+  // close form 'x' button
+  const closeForm = document.createElement('div');
+  closeForm.classList.toggle('closeForm');
+  closeForm.addEventListener('click', () => {
+    toggleForm();
+    removeForm();
+  });
+  closeForm.innerHTML = '&times';
+  overlayForm.appendChild(closeForm);
+  // form header
+  const formHeader = document.createElement('h1');
+  formHeader.textContent = 'Add a new ToDo';
+  overlayForm.appendChild(formHeader);
 
-    // form element 'Name' container 
-    const formElementName = document.createElement("div")
-    formElementName.classList.toggle("formElement")
-    overlayForm.appendChild(formElementName)
-    // 'Name' input fields - textr
-    const formNameLabel = document.createElement("label")
-    formNameLabel.setAttribute("for", "Name")
-    formNameLabel.textContent = "Name of ToDo";
-    const formNameInput = document.createElement("input")
-    formNameInput.setAttribute("type", "text")
-    formNameInput.setAttribute("id", "formName")
-    formNameInput.setAttribute("maxlength", "30")
-    formElementName.append(formNameLabel, formNameInput);
-    
-    // form 'Project' container
-    const formElementProject = document.createElement("div")
-    formElementProject.classList.toggle("formElement")
-    overlayForm.appendChild(formElementProject)
-    // 'Project' input fields - Dropdown
-    const formProjectLabel = document.createElement("label")
-    formProjectLabel.setAttribute("for", "projectInput")
-    formProjectLabel.textContent = "Select project:";
-    const formProjectInput = document.createElement("select")
-    formProjectInput.setAttribute("name", "projects")
-    formProjectInput.setAttribute("id", "projectInput")
-    formElementProject.append(formProjectLabel, formProjectInput);
-    // loop through project array to grab the projects for the dropdown
-    projectArray.forEach(function(element){
-        let option = document.createElement("option")
-        option.value = element;
-        option.textContent = `${element}`
-        formProjectInput.appendChild(option)
-    })
+  // form element 'Name' container
+  const formElementName = document.createElement('div');
+  formElementName.classList.toggle('formElement');
+  overlayForm.appendChild(formElementName);
+  // 'Name' input fields - textr
+  const formNameLabel = document.createElement('label');
+  formNameLabel.setAttribute('for', 'Name');
+  formNameLabel.textContent = 'Name of ToDo';
+  const formNameInput = document.createElement('input');
+  formNameInput.setAttribute('type', 'text');
+  formNameInput.setAttribute('id', 'formName');
+  formNameInput.setAttribute('maxlength', '30');
+  formElementName.append(formNameLabel, formNameInput);
 
-    // form 'priority' container
-    const formElementPriority = document.createElement("div")
-    formElementPriority.classList.toggle("formElement")
-    overlayForm.appendChild(formElementPriority)
-    // 'Priority' input fields - Dropdown
-    const formPriorityLabel = document.createElement("label")
-    formPriorityLabel.setAttribute("for", "priorityInput")
-    formPriorityLabel.textContent = "Select priority:";
-    const formPriorityInput = document.createElement("select")
-    formPriorityInput.setAttribute("name", "priority")
-    formPriorityInput.setAttribute("id", "priorityInput")
-    formElementProject.append(formPriorityLabel, formPriorityInput);
-    // priority dropdown options
-    const priorityUrgent = document.createElement("option")
-    priorityUrgent.setAttribute("value", "Urgent")
-    priorityUrgent.textContent = "Urgent"
-    const priorityRoutine = document.createElement("option")
-    priorityRoutine.setAttribute("value", "Routine")
-    priorityRoutine.textContent = "Routine"
-    const priorityBackBurner = document.createElement("option")
-    priorityBackBurner.setAttribute("value", "Backburner")
-    priorityBackBurner.textContent = "Back-Burner"
-    formPriorityInput.append(priorityUrgent, priorityRoutine, priorityBackBurner)
+  // form 'Project' container
+  const formElementProject = document.createElement('div');
+  formElementProject.classList.toggle('formElement');
+  overlayForm.appendChild(formElementProject);
+  // 'Project' input fields - Dropdown
+  const formProjectLabel = document.createElement('label');
+  formProjectLabel.setAttribute('for', 'projectInput');
+  formProjectLabel.textContent = 'Select project:';
+  const formProjectInput = document.createElement('select');
+  formProjectInput.setAttribute('name', 'projects');
+  formProjectInput.setAttribute('id', 'projectInput');
+  formElementProject.append(formProjectLabel, formProjectInput);
+  // loop through project array to grab the projects for the dropdown
+  projectArray.forEach((element) => {
+    const option = document.createElement('option');
+    option.value = element;
+    option.textContent = `${element}`;
+    formProjectInput.appendChild(option);
+  });
 
-    // form 'date due' container
-    const formElementDue = document.createElement("div")
-    formElementDue.classList.toggle("formElement")
-    overlayForm.appendChild(formElementDue)
-    // 'date due' input fields - date picker
-    const formDueLabel = document.createElement("label")
-    formDueLabel.setAttribute("for", "dueInput")
-    formDueLabel.textContent = "Due by:"
-    const formDueInput = document.createElement("input")
-    formDueInput.setAttribute("type", "date")
-    formDueInput.setAttribute("id", "dueInput")
-    formDueInput.setAttribute("name", "dueDate")
-    formDueInput.setAttribute("required", "")
-    formElementDue.append(formDueLabel, formDueInput)
+  // form 'priority' container
+  const formElementPriority = document.createElement('div');
+  formElementPriority.classList.toggle('formElement');
+  overlayForm.appendChild(formElementPriority);
+  // 'Priority' input fields - Dropdown
+  const formPriorityLabel = document.createElement('label');
+  formPriorityLabel.setAttribute('for', 'priorityInput');
+  formPriorityLabel.textContent = 'Select priority:';
+  const formPriorityInput = document.createElement('select');
+  formPriorityInput.setAttribute('name', 'priority');
+  formPriorityInput.setAttribute('id', 'priorityInput');
+  formElementProject.append(formPriorityLabel, formPriorityInput);
+  // priority dropdown options
+  const priorityUrgent = document.createElement('option');
+  priorityUrgent.setAttribute('value', 'Urgent');
+  priorityUrgent.textContent = 'Urgent';
+  const priorityRoutine = document.createElement('option');
+  priorityRoutine.setAttribute('value', 'Routine');
+  priorityRoutine.textContent = 'Routine';
+  const priorityBackBurner = document.createElement('option');
+  priorityBackBurner.setAttribute('value', 'Backburner');
+  priorityBackBurner.textContent = 'Back-Burner';
+  formPriorityInput.append(priorityUrgent, priorityRoutine, priorityBackBurner);
 
-    // form submit button container
-    const formElementSubmit = document.createElement("div")
-    formElementSubmit.classList.toggle("formElement")
-    overlayForm.appendChild(formElementSubmit)
-    // submit button 
-    const formSubmitButton = document.createElement("button")
-    formSubmitButton.textContent = "Add New Todo"
-    formSubmitButton.addEventListener("click", () => {
-        addTodo();
-        toggleForm();
-        removeForm();
-        clearTodos();
-        updateMain();
-        storeTodos();
-        console.log(todoArray)
-    }) 
+  // form 'date due' container
+  const formElementDue = document.createElement('div');
+  formElementDue.classList.toggle('formElement');
+  overlayForm.appendChild(formElementDue);
+  // 'date due' input fields - date picker
+  const formDueLabel = document.createElement('label');
+  formDueLabel.setAttribute('for', 'dueInput');
+  formDueLabel.textContent = 'Due by:';
+  const formDueInput = document.createElement('input');
+  formDueInput.setAttribute('type', 'date');
+  formDueInput.setAttribute('id', 'dueInput');
+  formDueInput.setAttribute('name', 'dueDate');
+  formDueInput.setAttribute('required', '');
+  formElementDue.append(formDueLabel, formDueInput);
 
-    formSubmitButton.setAttribute("type", "button")
-    formSubmitButton.setAttribute("id", "submit")
-    formElementSubmit.appendChild(formSubmitButton)
-    // append the entire form container to the body when called
-    document.getElementsByTagName("body")[0].appendChild(form)
-    console.log("todoForm function is firing")
+  // form submit button container
+  const formElementSubmit = document.createElement('div');
+  formElementSubmit.classList.toggle('formElement');
+  overlayForm.appendChild(formElementSubmit);
+  // submit button
+  const formSubmitButton = document.createElement('button');
+  formSubmitButton.textContent = 'Add New Todo';
+  formSubmitButton.addEventListener('click', () => {
+    addTodo();
+    toggleForm();
+    removeForm();
+    clearTodos();
+    updateMain();
+    storeTodos();
+    console.log(todoArray);
+  });
+
+  formSubmitButton.setAttribute('type', 'button');
+  formSubmitButton.setAttribute('id', 'submit');
+  formElementSubmit.appendChild(formSubmitButton);
+  // append the entire form container to the body when called
+  document.getElementsByTagName('body')[0].appendChild(form);
+  console.log('todoForm function is firing');
 }
 
 // function to toggle class that brings up a new todo form
-function toggleForm(){
-    document.body.classList.toggle("activeForm")
+function toggleForm() {
+  document.body.classList.toggle('activeForm');
 }
 // function to call when create a new Todo form button clicked, after completing the form
-function addTodo(name, project, priority, dueDate){
-    name = document.querySelector("#formName").value;
-    project = document.querySelector("#projectInput").value;
-    priority = document.querySelector("#priorityInput").value;
-    dueDate = document.querySelector("#dueInput").value;
-    const newTodo = new Todo(name,project,priority,dueDate)
-    newTodo.pushTodo();
+function addTodo(name, project, priority, dueDate) {
+  name = document.querySelector('#formName').value;
+  project = document.querySelector('#projectInput').value;
+  priority = document.querySelector('#priorityInput').value;
+  dueDate = document.querySelector('#dueInput').value;
+  const newTodo = new Todo(name, project, priority, dueDate);
+  newTodo.pushTodo();
 }
 
 // function to remove the form element from DOM when closed
 function removeForm() {
-    const elements = document.getElementsByClassName("form")
-    while (elements.length > 0){
-        elements[0].parentNode.removeChild(elements[0])
+  const elements = document.getElementsByClassName('form');
+  while (elements.length > 0) {
+    elements[0].parentNode.removeChild(elements[0]);
+  }
+}
+
+// **NEW PROJECT FORM
+function newProjectForm() {
+  // container elements for form
+  const form = document.createElement('form');
+  form.classList.toggle('form');
+  form.setAttribute('method', 'post');
+  form.setAttribute('action', '#');
+  const overlayForm = document.createElement('div');
+  overlayForm.classList.toggle('overlayForm');
+  form.appendChild(overlayForm);
+
+  // close form 'x' button
+  const closeForm = document.createElement('div');
+  closeForm.classList.toggle('closeForm');
+  closeForm.addEventListener('click', () => {
+    toggleForm();
+    removeForm();
+  });
+  closeForm.innerHTML = '&times';
+  overlayForm.appendChild(closeForm);
+
+  // form header
+  const formHeader = document.createElement('h1');
+  formHeader.textContent = 'Add a new Project';
+  overlayForm.appendChild(formHeader);
+
+  // project name input
+  const formElementName = document.createElement('div');
+  formElementName.classList.toggle('formElement');
+  overlayForm.appendChild(formElementName);
+  // 'Name' input fields - text
+  const formNameLabel = document.createElement('label');
+  formNameLabel.setAttribute('for', 'Name');
+  formNameLabel.textContent = 'Name of New Project';
+  const formNameInput = document.createElement('input');
+  formNameInput.required = true;
+  formNameInput.setAttribute('id', 'newProjectInput');
+  formNameInput.setAttribute('required', '');
+  formNameInput.setAttribute('name', 'newProjectInput');
+  formNameInput.setAttribute('minlength', '1');
+  formNameInput.setAttribute('maxlength', '30');
+  formElementName.append(formNameLabel, formNameInput);
+
+  // list of current Projects
+  const currentProjects = document.createElement('div');
+  currentProjects.classList.toggle('currentProjects');
+  overlayForm.appendChild(currentProjects);
+  const currentProjectsTitle = document.createElement('h2');
+  currentProjectsTitle.textContent = 'List of Current Projects';
+  currentProjects.append(currentProjectsTitle);
+  projectArray.forEach((element) => {
+    currentProjects.append(`${element}, `);
+  });
+
+  // submit new project button
+  const newProjectSubmit = document.createElement('div');
+  newProjectSubmit.classList.toggle('formElement');
+  overlayForm.appendChild(newProjectSubmit);
+  // submit button
+  const newProjectSubmitBtn = document.createElement('button');
+  newProjectSubmitBtn.textContent = '+ Add New Project';
+  newProjectSubmitBtn.addEventListener('click', () => {
+    // check that the input is valid, before running the toggle and remove functions
+    if (formNameInput.checkValidity()) {
+      pushNewProject();
+      toggleForm();
+      removeForm();
+      clearProjectList();
+      refreshProjectList();
+      storeProjects();
     }
-} 
+  });
 
-// **NEW PROJECT FORM 
-function newProjectForm(){
-    // container elements for form
-    const form = document.createElement("form")
-    form.classList.toggle("form")
-    form.setAttribute("method", "post")
-    form.setAttribute("action", "#")
-    const overlayForm = document.createElement("div")
-    overlayForm.classList.toggle("overlayForm")
-    form.appendChild(overlayForm)
+  newProjectSubmitBtn.setAttribute('type', 'submit');
+  newProjectSubmitBtn.setAttribute('id', 'submit');
+  newProjectSubmit.appendChild(newProjectSubmitBtn);
 
-    // close form 'x' button
-    const closeForm = document.createElement("div")
-    closeForm.classList.toggle("closeForm")
-    closeForm.addEventListener("click", () => {
-        toggleForm();
-        removeForm();
-    })
-    closeForm.innerHTML = "&times"
-    overlayForm.appendChild(closeForm)
-
-    // form header
-    const formHeader = document.createElement("h1")
-    formHeader.textContent = "Add a new Project"
-    overlayForm.appendChild(formHeader);
-
-    // project name input
-    const formElementName = document.createElement("div")
-    formElementName.classList.toggle("formElement")
-    overlayForm.appendChild(formElementName)
-    // 'Name' input fields - text
-    const formNameLabel = document.createElement("label")
-    formNameLabel.setAttribute("for", "Name")
-    formNameLabel.textContent = "Name of New Project";
-    const formNameInput = document.createElement("input")
-    formNameInput.required = true;
-    formNameInput.setAttribute("id", "newProjectInput")
-    formNameInput.setAttribute("required", "")
-    formNameInput.setAttribute("name", "newProjectInput")
-    formNameInput.setAttribute("minlength", "1")
-    formNameInput.setAttribute("maxlength", "30")
-    formElementName.append(formNameLabel, formNameInput);
-
-    // list of current Projects
-    const currentProjects = document.createElement("div")
-    currentProjects.classList.toggle("currentProjects")
-    overlayForm.appendChild(currentProjects)
-    const currentProjectsTitle = document.createElement("h2")
-    currentProjectsTitle.textContent = "List of Current Projects"
-    currentProjects.append(currentProjectsTitle)
-    projectArray.forEach((element) => {currentProjects.append(element + ", ")
-    })
-
-    // submit new project button
-    const newProjectSubmit = document.createElement("div")
-    newProjectSubmit.classList.toggle("formElement")
-    overlayForm.appendChild(newProjectSubmit)
-    // submit button 
-    const newProjectSubmitBtn = document.createElement("button")
-    newProjectSubmitBtn.textContent = "+ Add New Project"
-    newProjectSubmitBtn.addEventListener("click", () => {
-        // check that the input is valid, before running the toggle and remove functions
-        if (formNameInput.checkValidity()) {
-            pushNewProject();
-            toggleForm();
-            removeForm(); 
-            clearProjectList() 
-            refreshProjectList(); 
-            storeProjects();
-        }  
-    }) 
-
-    newProjectSubmitBtn.setAttribute("type", "submit")
-    newProjectSubmitBtn.setAttribute("id", "submit")
-    newProjectSubmit.appendChild(newProjectSubmitBtn)
-
-
-    // append form container to body
-    document.getElementsByTagName("body")[0].appendChild(form)
+  // append form container to body
+  document.getElementsByTagName('body')[0].appendChild(form);
 }
 
 function pushNewProject() {
-    const newProject = document.getElementById("newProjectInput")
-    projectArray.push(newProject.value)
+  const newProject = document.getElementById('newProjectInput');
+  projectArray.push(newProject.value);
 }
 export {
-    todoForm,
-    toggleForm,
-    removeForm,
-    newProjectForm
-}
+  todoForm,
+  toggleForm,
+  removeForm,
+  newProjectForm,
+};
--- src/localStorage.js
+++ src/localStorage.js
@@ -1,60 +1,54 @@
-import { refreshProjectList } from "./index"
-import updateMain from "./mainContent"
-import { projectArray, todoArray } from "./todoObjects"
+import { refreshProjectList } from './index';
+import updateMain from './mainContent';
+import { projectArray, todoArray } from './todoObjects';
 
-let todoJSONHolder = ""
-let projectJSONHolder = ""
-let localProjectArray = []
-let localTodoArray = []
-
-
-
+let todoJSONHolder = '';
+let projectJSONHolder = '';
+let localProjectArray = [];
+let localTodoArray = [];
 
 // retrieve the todos from local storage
-function retrieveTodos () {
-    todoJSONHolder = localStorage.getItem("storedTodos")
-    localTodoArray = JSON.parse(todoJSONHolder)
-    if (todoJSONHolder !== null){
-        localTodoArray.forEach(element => todoArray.push(element))
-        }
-        if (todoArray !== null) {
-            updateMain()   
-        }
-    }
+function retrieveTodos() {
+  todoJSONHolder = localStorage.getItem('storedTodos');
+  localTodoArray = JSON.parse(todoJSONHolder);
+  if (todoJSONHolder !== null) {
+    localTodoArray.forEach((element) => todoArray.push(element));
+  }
+  if (todoArray !== null) {
+    updateMain();
+  }
+}
 
 // retrieve the projects from local storage
-function retrieveProjects () {
-    projectJSONHolder = localStorage.getItem("storedProjects")
-    localProjectArray = JSON.parse(projectJSONHolder)
-    if (projectJSONHolder !== null){
-        localProjectArray.forEach(element => projectArray.push(element))
-        }
-        if (projectArray !== null) {
-            refreshProjectList()
-        }
-    }
-
+function retrieveProjects() {
+  projectJSONHolder = localStorage.getItem('storedProjects');
+  localProjectArray = JSON.parse(projectJSONHolder);
+  if (projectJSONHolder !== null) {
+    localProjectArray.forEach((element) => projectArray.push(element));
+  }
+  if (projectArray !== null) {
+    refreshProjectList();
+  }
+}
 
 // store the todos in local storage
-function storeTodos (){
-    if (todoArray !== null){
-        const todoJSON = JSON.stringify(todoArray);
-        localStorage.setItem("storedTodos", todoJSON)
-    } 
+function storeTodos() {
+  if (todoArray !== null) {
+    const todoJSON = JSON.stringify(todoArray);
+    localStorage.setItem('storedTodos', todoJSON);
+  }
 }
 // store the projects in local storage
-function storeProjects () {
-    if (projectArray !== null){
-        const projectJSON = JSON.stringify(projectArray);
-        localStorage.setItem("storedProjects", projectJSON)
-    }
+function storeProjects() {
+  if (projectArray !== null) {
+    const projectJSON = JSON.stringify(projectArray);
+    localStorage.setItem('storedProjects', projectJSON);
+  }
 }
-
-
 
 export {
-    storeTodos,
-    storeProjects,
-    retrieveTodos,
-    retrieveProjects,
-}
+  storeTodos,
+  storeProjects,
+  retrieveTodos,
+  retrieveProjects,
+};
